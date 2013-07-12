5fbe873b4ebd7653f371959a7d23bee7d258599a
Joao Sampaio <ubuntu@ip-172-31-32-111.us-west-2.compute.internal>
2013-07-12 06:55:05 +0000

    Altered web.js to read from index.html file.

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  fs.readFile('index.html',function(err,data){
+    if (err) throw err;
+    console.log(data);
+
+    var buf = new Buffer(data);
+    response.send(buf.toString('utf8',0,buf.length));
+  });
 });
 
 var port = process.env.PORT || 5000;
